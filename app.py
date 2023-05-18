2934de428a36f288b3ebf2f31fbe6af2b25b4c44
bangiroo <122073970+bangiroo@users.noreply.github.com>
2023-05-19 00:43:30 +0900

    app.py

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 import streamlit as st
 import openai
 
-openai.api_key = st.secrets['api_key']
+openai.api_key = st.secrets["api_key"]
 
-st.title('ChatGPT Plus DALL-E')
+st.title("ChatGPT Plus DALL-E")
 
-with st.form('form'):
-    user_input = st.text_input("프롬프트")
-    size = st.selectbox("사이즈", ["1024x1024", "512x512", "256x256"])
-    submit = st.form_submit_button('제출')
-
+with st.form("form"):
+    user_input = st.text_input("Prompt")
+    size = st.selectbox("Size", ["1024x1024", "512x512", "256x256"])
+    submit = st.form_submit_button("Submit")
 
 if submit and user_input:
     gpt_prompt = [{
-        'role' : 'system',
-        'content' : 'Imagine the detail appeareance of the input. Response it shortly around 20 words'
+        "role": "system",
+        "content": "Imagine the detail appeareance of the input. Response it shortly around 20 words"
     }]
 
     gpt_prompt.append({
-        'role' : 'user',
-        'content' : user_input
+        "role": "user",
+        "content": user_input
     })
 
     with st.spinner("Waiting for ChatGPT..."):
@@ -28,7 +27,7 @@ if submit and user_input:
             messages=gpt_prompt
         )
 
-    prompt = gpt_response['choices'][0]['message']['content']
+    prompt = gpt_response["choices"][0]["message"]["content"]
     st.write(prompt)
 
     with st.spinner("Waiting for DALL-E..."):
